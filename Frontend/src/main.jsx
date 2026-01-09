6279defd9be6d802b5495e397681738a096d778c
shenba <mshenba02@gmail.com>
2026-01-09 21:19:02 +0530

    Fix white screen: correct base path for GitHub Pages

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -6,7 +6,7 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/FullstackProjectUsingJava/">
       <App />
     </BrowserRouter>
   </React.StrictMode>
